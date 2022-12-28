4b9cf302dcd3836ef09118c51f52309c045401c5
Aleksandra <aleksandra@MacBook-Air-Aleksandra.local>
2022-12-28 16:28:04 +0300

    feat: add productDetails page

--- src/components/app/pages/productPage/productPageView.ts
+++ src/components/app/pages/productPage/productPageView.ts
@@ -1,13 +1,18 @@
 import { BaseComponent } from '../../../common/baseComponent';
+import { ProductDetails } from '../../view/productDetails/productDetails';
 
 class ProductPage extends BaseComponent {
+  productDetails: ProductDetails;
+
   constructor(id: number) {
     super('main', 'main', '');
+    this.productDetails = new ProductDetails(id);
   }
 
   public renderPage(): void {
-    const wrapperSection = new BaseComponent('div', 'product__page', 'ЭТО СТРАНИЦА ТОВАРА');
-    this.element.append(wrapperSection.element);
+    const wrapper = document.createElement('div');
+    wrapper.append(this.productDetails.element);
+    this.element.append(wrapper);
     document.querySelector('header')?.after(this.element);
   }
 }
