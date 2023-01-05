4d1074931860ae697b7d3208ac41e6412c6bcab7
Arzhanik <anastasia911@mail.ru>
2023-01-05 19:58:20 +0300

    fix: fixed loaded value slider

--- src/components/app/view/priceRange/priceRange.ts
+++ src/components/app/view/priceRange/priceRange.ts
@@ -5,15 +5,23 @@ import './priceRange.css';
 import data from '../../../../data';
 import Slider from '../slider/slider';
 import { getMaxMinValueByField } from '../../../common/utils';
+import { FilterController } from '../../controls/homePage/filterController';
+import { IFilters } from '../../../../types';
 
 class PriceRange extends BaseComponent {
+  filterController: FilterController;
+
   constructor(parentNode: HTMLElement) {
     super('div', 'price__range', '');
     this.renderView(parentNode);
+    this.filterController = new FilterController();
   }
 
   private renderView(parentNode: HTMLElement):void {
     const minMaxValue = getMaxMinValueByField('price', data);
+    const filters:IFilters = JSON.parse(localStorage.getItem('store-filter') as string);
+    const minValueLoad = filters ? filters.minPrice : minMaxValue[0];
+    const maxValueLoad = filters ? filters.maxPrice : minMaxValue[1];
     this.element.innerHTML = `
     <h3 class="price__title">Сортировать по цене</h3>
     <div class="price-slider-container">
@@ -25,7 +33,7 @@ class PriceRange extends BaseComponent {
     parentNode.append(this.element);
     const targetElement = <target> this.element.querySelector('#price-slider');
     const slider = new Slider();
-    slider.createSlider(targetElement, minMaxValue[0], minMaxValue[1]);
+    slider.createSlider(targetElement, minMaxValue[0], minMaxValue[1], minValueLoad, maxValueLoad);
   }
 }
 export default PriceRange;
